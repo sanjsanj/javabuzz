398b3c1fc391cb684b4404602defbd5c986102dc
sanjsanj <sanjsanj@hotmail.com>
2015-07-16 15:35:50 +0100

    refactor javabuzz functions into 1 isDivisibleBy function

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -7,29 +7,29 @@ describe('Javabuzz', function(){
 
   describe('knows when a number is', function(){
     it('divisible by 15', function(){
-      expect(javabuzz.isDivisiblebyFifteen(15)).toBe(true);
+      expect(javabuzz.isDivisibleBy(15, 15)).toBe(true);
     });
 
     it('divisible by 5', function(){
-      expect(javabuzz.isDivisiblebyFive(5)).toBe(true);
+      expect(javabuzz.isDivisibleBy(5, 5)).toBe(true);
     });
 
     it('divisible by 3', function(){
-      expect(javabuzz.isDivisiblebyThree(3)).toBe(true);
+      expect(javabuzz.isDivisibleBy(3, 3)).toBe(true);
     });
   });
 
   describe('knows when a number is NOT', function(){
     it('divisible by 15', function(){
-      expect(javabuzz.isDivisiblebyFifteen(2)).toBe(false);
+      expect(javabuzz.isDivisibleBy(2, 15)).toBe(false);
     });
 
     it('divisible by 5', function(){
-      expect(javabuzz.isDivisiblebyFive(2)).toBe(false);
+      expect(javabuzz.isDivisibleBy(2, 5)).toBe(false);
     });
 
     it('divisible by 3', function(){
-      expect(javabuzz.isDivisiblebyThree(2)).toBe(false);
+      expect(javabuzz.isDivisibleBy(2, 3)).toBe(false);
     });
   });
 });
