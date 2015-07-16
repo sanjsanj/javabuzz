2e64b6745dfbdf69fa6b8217663dd449ee82fe1c
sanjsanj <sanjsanj@hotmail.com>
2015-07-16 15:41:54 +0100

    refactor back in individual isDivisibleBy..s to simplify returning of Fizz, Buzz and Fizzbuzz

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -7,29 +7,29 @@ describe('Javabuzz', function(){
 
   describe('knows when a number is', function(){
     it('divisible by 15', function(){
-      expect(javabuzz.isDivisibleBy(15, 15)).toBe(true);
+      expect(javabuzz.isDivisibleByFifteen(15)).toBe(true);
     });
 
     it('divisible by 5', function(){
-      expect(javabuzz.isDivisibleBy(5, 5)).toBe(true);
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
     });
 
     it('divisible by 3', function(){
-      expect(javabuzz.isDivisibleBy(3, 3)).toBe(true);
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
   });
 
   describe('knows when a number is NOT', function(){
     it('divisible by 15', function(){
-      expect(javabuzz.isDivisibleBy(2, 15)).toBe(false);
+      expect(javabuzz.isDivisibleByFifteen(2)).toBe(false);
     });
 
     it('divisible by 5', function(){
-      expect(javabuzz.isDivisibleBy(2, 5)).toBe(false);
+      expect(javabuzz.isDivisibleByFive(2)).toBe(false);
     });
 
     it('divisible by 3', function(){
-      expect(javabuzz.isDivisibleBy(2, 3)).toBe(false);
+      expect(javabuzz.isDivisibleByThree(2)).toBe(false);
     });
   });
 });
--- src/JavaBuzz.js
+++ src/JavaBuzz.js
@@ -1,5 +1,17 @@
 var Javabuzz = function(){
-  Javabuzz.prototype.isDivisibleBy = function (number, divisor) {
+  Javabuzz.prototype.isDivisibleByFifteen = function (number) {
+    return this._isDivisibleBy(number, 15);
+  };
+
+  Javabuzz.prototype.isDivisibleByFive = function (number) {
+    return this._isDivisibleBy(number, 5);
+  };
+
+  Javabuzz.prototype.isDivisibleByThree = function (number) {
+    return this._isDivisibleBy(number, 3);
+  };
+
+  Javabuzz.prototype._isDivisibleBy = function (number, divisor) {
     return (number % divisor === 0);
   };
 };
